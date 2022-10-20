bb02019115900df308a389a0cbaab01122ca4dc4
IvorWithCrossbow <sergey.shin.t@gmail.com>
2022-10-20 20:57:49 +0600

    20.10

--- src/Validator.js
+++ src/Validator.js
@@ -4,7 +4,8 @@ class Validator {
 //   }
 
   validateUsername(name) {
-    const regex = /^[^\d\-_]{1, 1}[^\d{3}][A-z\-_\0-9][^\d\-_]{1, 1}$/g;
-    return regex.test(name);
+    if (/^[^\d\-_]{1, 1}[A-z\-_\d]*[^\d\-_]{1, 1}$/g.test(name)) {
+      return /[^\d{3}]/.test(name);
+    }
   }
 }
